1d49b3ef508e7edff58f67af5ce9351284c7af49
boithanh <boithanh01694@gmail.com>
2025-04-23 15:26:04 +0700

    add iframe random645custom

--- src/Components/MegaRandomCustom.jsx
+++ src/Components/MegaRandomCustom.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const MegaRandomCustom = () => {
     return (
-        <div>MegaRandomCustom</div>
+        <section className='d-flex justify-content-center align-items-center'>
+            <iframe src="https://mega-random-custom.vercel.app" frameborder="1" style={{ width: "100%", height: "100vh" }} ></iframe>
+        </section>
     )
 }
 
